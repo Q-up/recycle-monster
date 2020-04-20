cdf62b8fd805d89d548030eb5279f302c95948c4
Quincey Hollman <qhollman@gmail.com>
2020-04-20 12:50:05 -0700

    edits for heroku deployment

--- src/web.js
+++ src/web.js
@@ -1,6 +1,11 @@
 var express = require("express");
 var logfmt = require("logfmt");
+const path = require("path");
+
 var app = express();
+const static_path = path.join(__dirname, "../react-app", "build");
+console.log("Serving static from", static_path);
+app.use(express.static(static_path));
 
 app.use(logfmt.requestLogger());
 
