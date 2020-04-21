e76f9c04583071fb1430efbf339e94c4dfd36893
Quincey Hollman <qhollman@gmail.com>
2020-04-21 12:59:42 -0700

    edits to web.js for heroku deployment

--- src/web.js
+++ src/web.js
@@ -1,17 +1,24 @@
-var express = require("express");
-var logfmt = require("logfmt");
+const express = require("express");
+const logfmt = require("logfmt");
 const path = require("path");
+const bodyParser = require("body-parser");
+const app = express();
 
-var app = express();
 const static_path = path.join(__dirname, "../react-app", "build");
 console.log("Serving static from", static_path);
 app.use(express.static(static_path));
 
 app.use(logfmt.requestLogger());
 
-app.use("/", express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "react-app/build")));
+app.get("*", (_, res) => {
+  res.sendFile(path.join(__dirname + "/react-app/build/index.html"));
+});
 
-var port = Number(process.env.PORT || 3001);
-app.listen(port, function() {
-  console.log("Listening on " + port);
+var server = app.listen(process.env.PORT || 3001, function() {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log("my app is running at http://%s:%s", host, port);
 });
