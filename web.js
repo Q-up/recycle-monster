f7288069081659c06a0a0f0e0ef4eb09793a6dcc
Quincey Hollman <qhollman@gmail.com>
2020-04-22 10:40:23 -0700

    moved web.js and edits to further troubleshoot heroku

--- web.js
+++ web.js
@@ -13,12 +13,11 @@ app.use(logfmt.requestLogger());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "react-app/build")));
-app.get("*", (_, res) => {
+app.get("/recycle", (_, res) => {
   res.sendFile(path.join(__dirname + "/react-app/build/index.html"));
 });
 
-var server = app.listen(process.env.PORT || 3001, function() {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log("my app is running at http://%s:%s", host, port);
-});
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log(`Rycycle Monster listening on ${port}`);
