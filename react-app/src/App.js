4afe80563519a601542416fb0317762563c18a95
Peterson Trethewey <peterson@2-complex.com>
2020-05-05 13:28:28 -0700

    Fixed _some_ warnings

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,11 +3,6 @@ import PropTypes from "prop-types";
 import { withApp } from "react-pixi-fiber";
 import Game from "./components/GamePage";
 
-// TODO: make this DRY with index.js
-const rootDiv = document.getElementById("root");
-const height = rootDiv.height || 600;
-const width = rootDiv.width || 800;
-
 // This will be wrapped with withApp,
 // and imported, probably as App
 class WrappedApp extends Component {
--- react-app/src/components/Trash.js
+++ react-app/src/components/Trash.js
@@ -1,38 +1,43 @@
 import React, { Component } from "react";
 import { Sprite } from "react-pixi-fiber";
 import * as PIXI from "pixi.js";
-import DraggableContainer from "./DraggableContainer";
 
-function Trash(props) {
-  const loader = PIXI.Loader.shared;
-  const spriteAtlas = "/images/GameBackGround.json";
-  let sheet = loader.resources[spriteAtlas];
-  const centerAnchor = new PIXI.Point(0.5, 0.5);
-  const fijiBottle = sheet.textures["fiji-water-bottle.png"];
-  const plasticBag = sheet.textures["plasticbag.png"];
-  const burger = sheet.textures["burger.png"];
-  const trashTextures = [fijiBottle, plasticBag, burger];
-  // const onDragStart = () => {
-  //   console.log("On drag start", this);
-  // };
-  function getItems(array) {
+
+const centerAnchor = new PIXI.Point(0.5, 0.5);
+
+class Trash extends Component {
+
+  getItems(array) {
     return array.map((item) => (
       <Sprite
         interactive
-        pointerdown={props.pointerDown}
-        pointermove={props.pointerMove}
-        pointerup={props.pointerUp}
+        pointerdown={this.props.pointerDown}
+        pointermove={this.props.pointerMove}
+        pointerup={this.props.pointerUp}
         texture={item}
         anchor={centerAnchor}
         scale={0.4}
         y={-1}
         x={330}
-        {...props}
+        {...this.props}
       />
     ));
   }
 
-  return getItems(trashTextures);
+  render() {
+    const loader = PIXI.Loader.shared;
+    const spriteAtlas = "/images/GameBackGround.json";
+    let sheet = loader.resources[spriteAtlas];
+    const fijiBottle = sheet.textures["fiji-water-bottle.png"];
+    const plasticBag = sheet.textures["plasticbag.png"];
+    const burger = sheet.textures["burger.png"];
+    const trashTextures = [fijiBottle, plasticBag, burger];
+    // const onDragStart = () => {
+    //   console.log("On drag start", this);
+    // };
+
+    return this.getItems(trashTextures);
+  }
 }
 
 export default Trash;
